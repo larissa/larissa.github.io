c8d75cab7110829f16c905ee5ff26e045eb759cc
Larissa Reis <larissa@colivre.coop.br>
2015-05-04 12:43:01 -0300

    Add support to normalized tags

--- content/tags.md
+++ content/tags.md
@@ -3,3 +3,5 @@ title: Tags
 ---
 
 # Tags
+
+<%= render 'tags' %>
--- lib/default.rb
+++ lib/default.rb
@@ -4,7 +4,23 @@
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Tagging
 include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::Rendering
 
+require 'set'
+
+# Normalize all tags
+module NormalizeTags
+  def [](key)
+    result = super
+    key == :tags && !result.nil? ? result.map(&:downcase) : result
+  end
+end
+
+class Nanoc::Item
+  prepend NormalizeTags
+end
+
+# Helpers to format date
 def format_date(unformated_date)
   unformated_date.strftime('%B %e, %Y')
 end
