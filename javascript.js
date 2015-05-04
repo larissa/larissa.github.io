7f86bc8a815d2dabaa791aed2cb556602f0c6cff
Larissa Reis <larissa@colivre.coop.br>
2015-05-04 13:59:02 -0300

    Adds initial nanoc pages

---
+++ javascript.js
@@ -0,0 +1,5 @@
+function AbreTeSesamo(context) {
+    context.href = 'mxi' + 'lty:' + 'lxriss' + 'x@c' + 'ylivr' + 'e.cyy' + 'p.br';
+    context.href = context.href.replace(/x/g,'a');
+    context.href = context.href.replace(/y/g,'o');
+}
